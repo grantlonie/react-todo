7b9d0d2a805f3255f2217926385d8c9f72b578fd
Grant Lonie <glonie@autorobinc.com>
2018-09-13 09:39:05 -0500

    Update App with all todo components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,78 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			todos: ['pizza', 'sausage'],
+			value: '',
+			filterValue: '',
+		}
+	}
+
+	addTodo() {
+		const todos = [...this.state.todos]
+		todos.push(this.state.value)
+		this.setState({
+			todos,
+			value: '',
+			filterValue: '',
+		})
+	}
+
+	changeInputVal(e) {
+		this.setState({ value: e.target.value })
+	}
+
+	deleteTodo(todo) {
+		const todos = [...this.state.todos].filter(arrayTodo => todo !== arrayTodo)
+		this.setState({ todos })
+	}
+
+	changeFilterValue(e) {
+		this.setState({ filterValue: e.target.value })
+	}
+
+	render() {
+		const todos = this.state.todos.filter(todo => todo.indexOf(this.state.filterValue) > -1)
+
+		return (
+			<div>
+				Filter
+				<input
+					type="input"
+					value={this.state.filterValue}
+					onChange={this.changeFilterValue.bind(this)}
+				/>
+				<br />
+				<br />
+				<input type="input" value={this.state.value} onChange={this.changeInputVal.bind(this)} />
+				<button onClick={this.addTodo.bind(this)}>Add to do</button>
+				<Todos todos={todos} deleteTodo={this.deleteTodo.bind(this)} />
+			</div>
+		)
+	}
 }
 
-export default App;
+function Todos(props) {
+	return (
+		<div>
+			{props.todos.map(todo => (
+				<Todo val={todo} deleteTodo={props.deleteTodo} />
+			))}
+		</div>
+	)
+}
+
+function Todo(props) {
+	return (
+		<div>
+			<button onClick={() => props.deleteTodo(props.val)}>Done</button>
+			{props.val}
+		</div>
+	)
+}
+
+export default App
